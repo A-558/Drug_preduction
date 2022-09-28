bf1ab6266968ed71862ec5e62d7df84129f9dabe
Akhil <81339780+A-558@users.noreply.github.com>
2022-09-28 13:56:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['POST','GET'])
 def home():
     return render_template('index.html')
 
